e426b1def5188e7fa5f63f5756c429c7c9d4ef21
Shadiyah Mangru <shadiyahmangru@gmail.com>
2019-02-20 12:29:25 -0500

    Update Stream.java

--- Stream.java
+++ Stream.java
@@ -49,7 +49,7 @@ class HockeyPlayer{
 	}
 }
 
-class Skater extends HockeyPlayer implements Comparable<Skater>{
+class Skater extends HockeyPlayer{
 	//fields
 	private int goals;
 	private int shots;
@@ -61,12 +61,6 @@ class Skater extends HockeyPlayer implements Comparable<Skater>{
 		this.goals = goals;
 		this.shots = shots;
 		this.shootingPercent = (float)0;
-	}
-	
-	//this sorts skaters by goals scored, in descending order
-	@Override
-    	public int compareTo(Skater other) {
-    		return (int)(other.getGoals() - this.getGoals());
 	}
 	
 	//setters
@@ -110,43 +104,31 @@ class Roster{
 		roster = new ArrayList<>();
 		try{
 			//.txt file that stores data needed for calculations
-			File file = new File("C:\\Users\\593476\\Desktop\\Java Programs\\roster2018Goals_Shots_Stats2_14_2019.txt"); 
+			File file = new File("C:\\Users\\593476\\Desktop\\Java Programs\\roster2018Goals_Shots_Stats2_19_2019.txt"); 
     			Scanner sc = new Scanner(file); 
-    			int counter = 0;
-    			String name = "";
-    			String position = "";
     			int jersey = 0;
     			int goals = 0;
     			int shots = 0;
+    			String [] gs = {"", ""};
     			while (sc.hasNextLine()) {
-    				String getData = sc.nextLine();
-    				if(counter==0){
-    					name = getData.trim();
-    				}
-    				else if(counter==1){
-    					position = getData.trim();	
-    				}
-    				else if(counter==2){
-    					jersey=Integer.parseInt(getData.trim());	
-    				}
-    				else if(counter>=3 && (!getData.equals("*")) && !position.equals("Goalie")){
-    					String [] gs = getData.trim().split(":");
+    				String name = sc.nextLine().trim();
+    				String position  = sc.nextLine().trim();
+    				jersey = Integer.parseInt(sc.nextLine().trim());
+    				if(!position.equals("Goalie")){
+    					gs = sc.nextLine().trim().split(":");
     					goals = Integer.parseInt(gs[0]);
     					shots = Integer.parseInt(gs[1]);
+    					HockeyPlayer hp = new HockeyPlayer(name, position, jersey);
+    					Skater s = new Skater(hp, goals, shots);
+    					roster.add(s);
+    					sc.nextLine();   //skip *
 				}
-				counter++;
-    				if(getData.equals("*")){
-    					if(position.equals("Goalie")){
-    						HockeyPlayer hp = new HockeyPlayer(name, position, jersey);
-    						roster.add(hp);
-    					}
-    					else{
-    						HockeyPlayer hp = new HockeyPlayer(name, position, jersey);
-    						Skater s = new Skater(hp, goals, shots);
-    						roster.add(s);
-    					}
-    					counter = 0;
-    				}
+				else{
+					HockeyPlayer hp = new HockeyPlayer(name, position, jersey);
+    					roster.add(hp);
+    					sc.nextLine();	 //skip 0:0 for Goalies
+					sc.nextLine();   //skip *
+				}
     			}
     		}
     		catch(Exception e){
@@ -164,79 +146,99 @@ class Roster{
 	}
 }
 
-public class Stream{
+interface SkaterComparisons{
+	//this method sorts in descending order by goals scored; 
+	//in the event of a tie, players are sorted in ascending order by last name.
+	//parameters must accept a narrowing cast to Skater for valid results
+	public static int compareByGoalsThenName(HockeyPlayer h1, HockeyPlayer h2){
+		try{
+			Skater s1 = (Skater)h1;
+			Skater s2 = (Skater)h2;
+			if(s2.getGoals() - s1.getGoals() != 0){
+				return s2.getGoals() - s1.getGoals();	
+			}
+			return s1.getLastName().compareTo(s2.getLastName());	
+		}
+		catch(ClassCastException cce){
+			System.out.println("Exception: " + cce);	
+		}
+		return 0;
+	}	
+}
+
+interface Lambdas{
 	//the BiFunction Functional Interface turns two parameters into a value of a potentially different type and returns it
-	private static BiFunction<Integer, Integer, Float> shootPer = (g, s) -> {
+	//this method accepts a player's goals and shots as parameters, and returns the player's shooting percentage
+	public static BiFunction<Integer, Integer, Float> shootPer = (g, s) -> {
 		if(s == 0){
 			return (float)0;
 		}
-		else{
-			return ((float)g / (float)s)*100;
-		}		
+		return ((float)g / (float)s)*100;		
 	};
 	
 	//the BiConsumer Functional Interface accepts/manipulates two parameters, but does not return anything
-	private static BiConsumer<Float, Skater> setShootPer = (sp, sk) -> {
+	//this method accepts a shooting percentage and a skater, and sets that skater object's shooting percentage field
+	public static BiConsumer<Float, Skater> setShootPer = (sp, sk) -> {
 		sk.setShootingPercent(sp);	
 	};
 	
 	//the Predicate Functional Interface may be used to test a condition (often used when filtering or matching)
-	//this returns true if the HockeyPlayer is not a Goalie
-	private static Predicate<HockeyPlayer> filterOutGoalies = hp -> {
+	//this method accepts a HockeyPlayer parameter and returns true if the HockeyPlayer is not a Goalie
+	public static Predicate<HockeyPlayer> filterOutGoalies = hp -> {
 		if(!hp.getPosition().equals("Goalie")){
 			return true;	
 		}
-		else{
-			return false;	
-		}
+		return false;	
 	};
 	
-	//this returns true if the HockeyPlayer wears an even-numbered jersey
-	private static Predicate<HockeyPlayer> evenNumberPlayers = hp -> {
+	//this method accepts a HockeyPlayer parameter and returns true if the HockeyPlayer wears an even-numbered jersey
+	public static Predicate<HockeyPlayer> evenNumberPlayers = hp -> {
 		if(hp.getJersey() % 2 == 0){
 			return true;	
 		}
-		else{
-			return false;	
-		}
+		return false;	
 	};
 	
 	//the Supplier Functional Interface may be used to generate or supply values without taking any input
-	private static Supplier<String> assignTeam = () -> {
+	//this method takes no parameters and returns the String "WSH".
+	public static Supplier<String> assignTeam = () -> {
 		return "WSH";
 	};
+}
 
+public class Stream{
+	//Comparator that sorts HockeyPlayers (who are Not Goalies) by goals then lastname, written with a Java SE 8 method reference
+	static Comparator<HockeyPlayer> byGoalsThenName = SkaterComparisons :: compareByGoalsThenName;
 	
 	//this method relies on Supplier F.I. to set team field of hockeyplayer object, Predicate F.I. to filter out goalies, 
 	//BiFunction F.I. to calculate skater's shooting percentage, and BiConsumer F.I. 
 	//to set the shootingPercent field of the skater object (in case this value needed later).
 	private static void printSKShootPercent(HockeyPlayer hp){
-		hp.setTeam(assignTeam.get()); //calls Supplier
-		if(filterOutGoalies.test(hp)){ //calls Predicate
-			Skater s = (Skater)hp;
-			Float calcSP = shootPer.apply(s.getGoals(), s.getShots()); //calls BiFunction
-			setShootPer.accept(calcSP, s); //calls BiConsumer
+		hp.setTeam(Lambdas.assignTeam.get()); //calls Supplier
+		if(Lambdas.filterOutGoalies.test(hp)){ //calls Predicate
+			Skater s = (Skater)hp; //narrowing cast of HockeyPlayer object to a Skater object
+			Float calcSP = Lambdas.shootPer.apply(s.getGoals(), s.getShots()); //calls BiFunction
+			Lambdas.setShootPer.accept(calcSP, s); //calls BiConsumer
 			System.out.println(String.format("| %-4s | %-15s | %-4s | %-7s | %-15s |", s.getTeam(), s.getLastName(), s.getJersey(), s.getGoals() , s.getShootingPercent()));
 		}
 	}
 	
-	//main method that prints (to console) the stat chart of the current roster
+	//main method that prints (to console) the sorted stat chart of the current roster
 	public static void main(String... args){
 		Roster r = new Roster();
-		ArrayList<HockeyPlayer> team = new ArrayList<>();
-		for(int i = 0; i< r.getRosterCount(); i++){
+		ArrayList<HockeyPlayer> team = new ArrayList<>(); //initialize an ArrayList 
+		for(int i = 0; i< r.getRosterCount(); i++){				//with HockeyPlayer objects of current roster
 			team.add(r.getHockeyPlayer(i));
 		}
 		
-		System.out.println("\n******* WSH Top Ten Goal Scorers Who Wear Even-Numbered Jerseys (since 2/14/2019) *******\n");
+		System.out.println("\n******* Goals Scored By WSH Non-Goalies Who Wear Even-Numbered Jerseys (since 2/19/2019) *******\n");
 		System.out.println(String.format("| %-4s | %-15s | %-4s | %-7s | %-15s |", "Team", "Player", "#", "Goals", "Shooting %"));
 		System.out.println("---------------------------------------------------------------");
 		team.stream()
-		.filter(hp -> filterOutGoalies.test(hp)) //calls Predicate
-		.filter(hp -> evenNumberPlayers.test(hp)) //calls Predicate
-		.sorted()
-		.limit(10)
-		.forEach(hp -> printSKShootPercent(hp));
+		.filter(Lambdas.filterOutGoalies :: test) //calls Predicate w/a method reference
+		.filter(Lambdas.evenNumberPlayers :: test) //calls Predicate w/a method reference
+		.sorted(byGoalsThenName) //sort stream with a Comparator!
+		.forEach(Stream :: printSKShootPercent); //calls printSKShootPercent with a method reference
 		System.out.println("\n****************************************************************");		
 	}
 }
